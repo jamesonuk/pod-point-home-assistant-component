cbd659a0e09f90d2cc8df26c01bea1ede52e6b65
Jameson_UK <1040621+jamesonuk@users.noreply.github.com>
2025-05-12 16:02:44 +0100

    And some more deprecation

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -3,7 +3,8 @@
 from types import MappingProxyType
 from unittest.mock import patch
 
-from homeassistant import config_entries, data_entry_flow
+from homeassistant import config_entries
+from homeassistant.data_entry_flow import FlowResultType
 from homeassistant.helpers.service_info  import dhcp
 from homeassistant.core import HomeAssistant
 import pytest
@@ -59,7 +60,7 @@ async def test_successful_config_flow(hass, bypass_get_data):
     )
 
     # Check that the config flow shows the user form as the first step
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["type"] == FlowResultType.FORM
     assert result["step_id"] == "user"
 
     # If a user were to enter `test_username` for username and `test_password`
@@ -70,7 +71,7 @@ async def test_successful_config_flow(hass, bypass_get_data):
 
     # Check that the config flow is complete and a new entry is created with
     # the input data
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["type"] == FlowResultType.CREATE_ENTRY
     assert result["title"] == "test@example.com"
     assert result["data"] == MOCK_CONFIG
     assert result["result"]
@@ -91,7 +92,7 @@ async def test_reauth_config_flow(hass, bypass_get_data):
     )
 
     # Check that the config flow shows the reauth form as the first step
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["type"] == FlowResultType.FORM
     assert result["step_id"] == "reauth_confirm"
 
     # If a user were to confirm the re-auth start, this function call
@@ -100,7 +101,7 @@ async def test_reauth_config_flow(hass, bypass_get_data):
     )
 
     # It should load the user form
-    assert result_2["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result_2["type"] == FlowResultType.FORM
     assert result_2["step_id"] == "user"
 
     updated_config = MOCK_CONFIG
@@ -113,7 +114,7 @@ async def test_reauth_config_flow(hass, bypass_get_data):
 
     # Check that the config flow is complete and a new entry is created with
     # the input data
-    assert result_3["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result_3["type"] == FlowResultType.CREATE_ENTRY
     assert result_3["title"] == "test@example.com"
     assert result_3["data"] == MOCK_CONFIG
     assert result_3["result"]
@@ -130,14 +131,14 @@ async def test_failed_config_flow(hass, error_on_get_data):
         DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
 
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["type"] == FlowResultType.FORM
     assert result["step_id"] == "user"
 
     result = await hass.config_entries.flow.async_configure(
         result["flow_id"], user_input=MOCK_CONFIG
     )
 
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["type"] == FlowResultType.FORM
     assert result["errors"] == {"base": "auth"}
 
 
@@ -154,7 +155,7 @@ async def test_options_flow(hass, bypass_get_data):
     result = await hass.config_entries.options.async_init(entry.entry_id)
 
     # Verify that the first options step is a user form
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["type"] == FlowResultType.CREATE_ENTRY
     assert result["step_id"] == "user"
 
     # Enter some fake data into the form
@@ -164,7 +165,7 @@ async def test_options_flow(hass, bypass_get_data):
     )
 
     # Verify that the flow finishes
-    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["type"] == FlowResultType.CREATE_ENTRY
     assert result["title"] == "test@example.com"
 
     # Verify that the options were updated
@@ -190,7 +191,7 @@ async def test_dhcp_flow(hass: HomeAssistant, bypass_get_data) -> None:
         data=DHCP_SERVICE_INFO,
         context={"source": config_entries.SOURCE_DHCP},
     )
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["type"] == FlowResultType.FORM
     assert result["step_id"] == "user"
 
     result2 = await hass.config_entries.flow.async_configure(
@@ -209,7 +210,7 @@ async def test_dhcp_login_error(hass: HomeAssistant, bypass_get_data) -> None:
         data=DHCP_SERVICE_INFO,
         context={"source": config_entries.SOURCE_DHCP},
     )
-    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["type"] == FlowResultType.FORM
     assert result["step_id"] == "user"
     with patch(
         "podpointclient.client.PodPointClient.async_credentials_verified",
