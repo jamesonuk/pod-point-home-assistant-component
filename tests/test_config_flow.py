25179077f1c12e50d2d0e18ac7569452e57e0dd4
Jameson_UK <1040621+jamesonuk@users.noreply.github.com>
2025-06-02 10:49:43 +0100

    Revert "fix async issue"
    This reverts commit da0ec80fb381fe75705a20e41012077b2d99cc0b.

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -87,7 +87,7 @@ async def test_reauth_config_flow(hass, bypass_get_data):
     entry.add_to_hass(hass)
 
     # Initialize a config flow
-    result = entry.async_start_reauth(hass)
+    result = await entry.async_start_reauth(hass)
 
     # Check that the config flow shows the reauth form as the first step
     assert result["type"] == FlowResultType.FORM
